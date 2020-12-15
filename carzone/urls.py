eaf1809ee5a39c3e950fbf1e42221ed633f8a700
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2020-12-15 23:05:18 +0530

    Added cars app

--- carzone/urls.py
+++ carzone/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 import pages
+import cars
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('cars', include('cars.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
